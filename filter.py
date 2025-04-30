16ab383cb8c3b627e843ac598a0e6e178b14b286
archhcra <bomcom@bk.ru>
2025-04-30 19:23:15 +0300

    4_test

--- filter.py
+++ filter.py
@@ -1,24 +1,23 @@
 import sys
 import string
 
-# Получаем путь к input.txt из аргументов командной строки
+
 input_file_path = sys.argv[1]
 
-# Открываем файл input.txt для чтения
-with open(input_file_path, 'r', encoding='utf-8') as file:
-    # Читаем содержимое файла
-    text = file.read()
 
-# Разбиваем текст на слова и удаляем знаки препинания
+with open(input_file_path, 'r', encoding='utf-8') as file:
+   text = file.read()
+
+
 words = text.split()
 cleaned_words = [word.strip(string.punctuation) for word in words]
 
-# Ищем слова, содержащие '23'
+
 words_with_23 = [word for word in cleaned_words if '23' in word]
 
-# Сохраняем найденные слова в новый файл output.txt
+
 with open('output.txt', 'w', encoding='utf-8') as output_file:
     for word in words_with_23:
         output_file.write(word + '\n')
 
-print("Слова, содержащие '23', были сохранены в output.txt.")
+print("'23', save in output.txt.")
--- test_script.py
+++ test_script.py
@@ -2,22 +2,17 @@ import os
 import tempfile
 
 def test_words_with_23():
-    # Создаем временный файл для input.txt
     with tempfile.NamedTemporaryFile(delete=False, mode='w', encoding='utf-8') as temp_input:
         temp_input.write("This is a test 23word and another 23example.")
         temp_input_path = temp_input.name
 
-    # Запускаем основной код
-    os.system(f'python filter.py {temp_input_path}')  # Передаем путь к временно созданному файлу
+    os.system(f'python filter.py {temp_input_path}') 
 
-    # Проверяем output.txt
     with open('output.txt', 'r', encoding='utf-8') as f:
         output_words = f.read().splitlines()
 
-    # Проверяем, что в output.txt содержатся правильные слова
     assert '23word' in output_words
     assert '23example' in output_words
-    assert len(output_words) == 2  # Должно быть 2 слова
+    assert len(output_words) == 2
 
-    # Удаляем выходной файл
     os.remove('output.txt')
